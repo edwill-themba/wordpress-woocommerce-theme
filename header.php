1acd757edd799c3170dc50514817dcf9e729641f
edwill themba <mpephothemba@gmail.com>
2021-10-02 08:28:49 +0100

    readme complete

--- header.php
+++ header.php
@@ -6,17 +6,26 @@
 </head>
 <body <?php body_class('test'); ?>>
 <header>
-	<div class="logo">
-      <a href="<?php echo esc_url(home_url()) ?>" class="brand">
-		<?php bloginfo('name') ?>
-	  </a>
-	</div>
-	<div class="navigation">
-	    <?php
-	      wp_nav_menu( array(
-		         'menu'           => 'primary',
-                 'theme_location' => 'primary',
-		  ));
-		?>
-	</div>
+	<nav>
+		 <div class="logo">
+	     <a href="<?php echo esc_url(home_url()) ?>" class="brand">
+	      	<?php bloginfo('name') ?>
+	     </a>
+     </div>
+  	<div class="navigation">
+	     <?php
+	        wp_nav_menu( array(
+		          'menu'           => 'primary',
+              'theme_location' => 'primary',
+		       ));
+	   	  ?>
+		</div>
+		<div class="toggle">
+		  <input type="checkbox" class="checkbox" id="checkbox">
+		    <label for="checkbox">
+			   <i class="fas fa-bars bars"></i>
+			   <i class="fas fa-times cancel"></i>
+		   </label>
+		</div>
+	 </nav> 
 </header>
---
+++ readme.md
@@ -0,0 +1,3 @@
+# MET Powders Theme
+ This is a complete theme ecommerce for online store,but you can add more functionality in terms of
+ woocommerce and in terms of your store requirements,you can also customize it put your own names and images if you wish,please feel freeto customize the themes
